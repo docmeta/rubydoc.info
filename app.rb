451886e38e97c945408a1a4addbbdec94f6d1d6a
Loren Segal <lsegal@soen.ca>
2010-08-28 21:11:10 -0400

    Expand path before pushing, 1.9.2 support

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
-$:.unshift(File.dirname(__FILE__))
-$:.unshift(File.dirname(__FILE__) + '/lib')
-$:.unshift(File.dirname(__FILE__) + '/yard/lib')
+$:.unshift(File.expand_path(File.dirname(__FILE__)))
+$:.unshift(File.expand_path(File.dirname(__FILE__) + '/lib'))
+$:.unshift(File.expand_path(File.dirname(__FILE__) + '/yard/lib'))
 
 require 'yard'
 require 'sinatra'
