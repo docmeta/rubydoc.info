5d0de48ad234a642c5940ef9231d6dc0f8284650
Loren Segal <lsegal@soen.ca>
2011-03-27 14:02:13 -0400

    Remove frames.html for new gems

--- scripts/update_remote_gems.rb
+++ scripts/update_remote_gems.rb
@@ -29,8 +29,28 @@ Gem::SpecFetcher.fetcher.list(true).values.flatten(1).each do |info|
   (libs[info[0]] ||= []) << GemVersion.new(*info)
 end
 
+# Keep track of updates gems
+changed_gems = {}
+File.readlines(REMOTE_GEMS_FILE).each do |line|
+  name, rest = line.split(/\s+/, 2)
+  changed_gems[name] = rest
+end
+
 File.open(REMOTE_GEMS_FILE, 'w') do |file|
   libs.each do |k, v|
-    file.puts("#{k} #{pick_best_versions(v).join(' ')}")
+    line = pick_best_versions(v).join(' ')
+    changed_gems.delete(k) if changed_gems[k].strip == line.strip
+    file.puts("#{k} #{line}")
   end
+end
+
+# Clear cache for gem frames page with new gems
+changed_gems.keys.each do |gem|
+  file = File.join(STATIC_PATH, 'gems', gem, 'frames.html')
+  `rm #{file}` if File.file?(file)
+end
+
+if changed_gems.size > 0
+  puts ">> Updated #{changed_gems.size} gems:" 
+  puts changed_gems.keys.join(', ')
 end
