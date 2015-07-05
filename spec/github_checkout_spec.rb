c594a0bb8e1f7ac3f6c39b34d15693b775be87c4
takiy33 <takiy33@gmail.com>
2015-07-05 23:21:40 +0900

    Convert specs to RSpec 2.99
    This conversion is done by Transpec 3.1.0.

--- spec/github_checkout_spec.rb
+++ spec/github_checkout_spec.rb
@@ -15,53 +15,53 @@ describe GithubCheckout do
     %w(git http https).each do |scheme|
       it "should accept github URLs with #{scheme}://" do
         git("#{scheme}://github.com/lsegal/yard")
-        @git.username.should == "lsegal"
-        @git.project.should == "yard"
-        @git.name.should == "lsegal/yard"
+        expect(@git.username).to eq("lsegal")
+        expect(@git.project).to eq("yard")
+        expect(@git.name).to eq("lsegal/yard")
       end
     end
 
     it "should accept github URLs with ending in .git" do
       git("git://github.com/lsegal/yard.git")
-      @git.username.should == "lsegal"
-      @git.project.should == "yard"
-      @git.name.should == "lsegal/yard"
+      expect(@git.username).to eq("lsegal")
+      expect(@git.project).to eq("yard")
+      expect(@git.name).to eq("lsegal/yard")
     end
 
     it "should sanitize project names" do
       git("git://github.com/foo!/bar!")
-      @git.name.should == "foo_/bar_"
+      expect(@git.name).to eq("foo_/bar_")
     end
 
     it "should sanitize SHA-1 commit" do
       @git = GithubCheckout.new(@settings, "git://github.com/lsegal/yard", "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3")
-      @git.commit.should == "a94a8f"
+      expect(@git.commit).to eq("a94a8f")
     end
 
     it "should use master as commit if not selected" do
       git("git://github.com/lsegal/yard")
-      @git.commit.should == "master"
+      expect(@git.commit).to eq("master")
     end
 
     it "should throw InvalidSchemeError on non github URL" do
       %w( http:// git:// sgdfhij gi://github.com/lsegal/yard git://github.com
         git://github.com/lsegal/ ).each do |url|
-          lambda { GithubCheckout.new(@settings, url) }.should raise_error(InvalidSchemeError)
+          expect { GithubCheckout.new(@settings, url) }.to raise_error(InvalidSchemeError)
       end
     end
   end
 
   describe '#is_fork?' do
     it "should return false for master repo" do
-      File.should_receive(:directory?).and_return(false)
+      expect(File).to receive(:directory?).and_return(false)
       git("git://github.com/lsegal/yard")
-      @git.should_not be_fork
+      expect(@git).not_to be_fork
     end
 
     it "should return true for non-master repo" do
-      File.should_receive(:directory?).and_return(false)
+      expect(File).to receive(:directory?).and_return(false)
       git("git://github.com/lsegal/redcarpet")
-      @git.should be_fork
+      expect(@git).to be_fork
     end
   end
 end
