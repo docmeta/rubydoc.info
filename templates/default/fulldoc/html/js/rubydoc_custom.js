6ca3d4478e8c1008e7492337f2c5c712cc7e02f6
David Alexander <davidpaulalexander@gmail.com>
2015-11-24 13:35:37 -0500

    Fixes github link generation to pull commit from breadcrumbs
    Pulling from URL has issue if it's not present there. Changing the url
    to have a label saying it's at a certain version breaks the conventions
    for the Stdlib and Gem url structure conventions, so we should instead
    just use the version already output to the page.

--- templates/default/fulldoc/html/js/rubydoc_custom.js
+++ templates/default/fulldoc/html/js/rubydoc_custom.js
@@ -1,7 +1,7 @@
 function createGithubLinks() {
-  if (match = window.location.pathname.match(/^\/github\/([^\/]+\/[^/]+)\/([^/]+)/)) {
+  if (match = window.location.pathname.match(/^\/github\/([^\/]+\/[^/]+)\/(?:[^/]+)/)) {
     var github_project = match[1];
-    var github_commit = match[2];
+    var github_commit = $('#menu .title small').text().replace(/^\(|\)\s*$/g, '');
 
     $(".source_code").each( function() {
        if (match = $(this).find(".info.file").text().match(/^# File '([^']+)', line (\d+)/)) {
