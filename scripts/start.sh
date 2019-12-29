e38fad642299bf1541fa7ce0e71c3553e156a6a6
Loren Segal <lsegal@soen.ca>
2019-12-28 18:51:44 -0800

    Force stack deploy

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,4 +5,4 @@ ruby scripts/setup_host_path.rb
 chown root:docker /var/run/docker.sock 2>/dev/null
 docker swarm init 2>/dev/null
 docker-compose build
-docker stack deploy --compose-file docker-compose.yml rubydoc
+docker stack deploy --resolve-image never --compose-file docker-compose.yml rubydoc
