0250b2d2695a61af165f0b52cb0878de44d6d0b9
Loren Segal <lsegal@soen.ca>
2020-06-30 18:42:12 -0700

    Generate GitHub links from library version info in template
    Fixes #91

--- templates/default/fulldoc/html/js/rubydoc_custom.js
+++ templates/default/fulldoc/html/js/rubydoc_custom.js
@@ -1,22 +1,19 @@
 function createGithubLinks() {
-  if (match = window.location.pathname.match(/^\/github\/([^\/]+\/[^/]+)\/([^/]+)/)) {
-    var github_project = match[1];
-    var github_commit = match[2];
+  if (window.location.pathname.indexOf('/github/') !== 0) return;
 
-    $(".source_code").each( function() {
-       if (match = $(this).find(".info.file").text().match(/^# File '([^']+)', line (\d+)/)) {
-          var file = match[1];
-          var line = match[2];
+  $(".source_code").each(function () {
+    if (match = $(this).find(".info.file").text().match(/^# File '([^']+)', line (\d+)/)) {
+      var file = match[1];
+      var line = match[2];
 
-          var url = "https://github.com/" + github_project +
-                    "/blob/" + github_commit + "/" +
-                    file +
-                    "#L" + line;
+      var url = "https://github.com/" + window.yard_library_name +
+                "/blob/" + window.yard_library_version + "/" +
+                file +
+                "#L" + line;
 
-          $(this).before(' [<a target="_new" href="' + url + '">View on GitHub</a>]');
-        }
-    });
-  }
+      $(this).before(' [<a target="_new" href="' + url + '">View on GitHub</a>]');
+    }
+  });
 }
 
 $(createGithubLinks);
