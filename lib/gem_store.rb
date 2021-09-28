3c787bff584d11472a53a4320998e7020111ffa2
Loren Segal <lsegal@soen.ca>
2021-09-28 00:36:09 -0700

    Retryable db

--- lib/gem_store.rb
+++ lib/gem_store.rb
@@ -20,57 +20,78 @@ class GemStore
 
   include Enumerable
 
-  def [](name) to_versions(RemoteGem.first(name_eq(name))) end
+  def [](name) retryable { to_versions(RemoteGem.first(name_eq(name))) } end
   def []=(name, versions)
-    versions = versions.split(' ') if versions.is_a?(String)
-    versions = versions.map {|v| v.is_a?(YARD::Server::LibraryVersion) ? v.version : v }
-    versions = VersionSorter.sort(versions)
-    if RemoteGem.where(name_eq(name)).count > 0
-      RemoteGem.first(name_eq(name)).update(versions: versions.join(" "))
-    else
-      RemoteGem.create(name: name, versions: versions.join(" "))
+    retryable do
+      versions = versions.split(' ') if versions.is_a?(String)
+      versions = versions.map {|v| v.is_a?(YARD::Server::LibraryVersion) ? v.version : v }
+      versions = VersionSorter.sort(versions)
+      if RemoteGem.where(name_eq(name)).count > 0
+        RemoteGem.first(name_eq(name)).update(versions: versions.join(" "))
+      else
+        RemoteGem.create(name: name, versions: versions.join(" "))
+      end
     end
-  rescue Sequel::DatabaseError => e
-    puts "Database error when writing versions (locked?): #{e.message}"
   end
 
   def delete(name)
-    RemoteGem.where(name_eq(name)).delete
+    retryable { RemoteGem.where(name_eq(name)).delete }
   end
 
-  def has_key?(name) !!RemoteGem.first(name_eq(name)) end
-  def each(&block) RemoteGem.each {|row| yield row.name, to_versions(row) } end
-  def size; RemoteGem.count end
+  def has_key?(name) retryable { !!RemoteGem.first(name_eq(name)) } end
+  def each(&block) retryable { RemoteGem.each {|row| yield row.name, to_versions(row) } } end
+  def size; retryable { RemoteGem.count } end
   def empty?; size == 0 end
 
   def pages_of_letter(letter)
-    (RemoteGem.where(Sequel.like(:name, "#{letter}%")).count / PER_PAGE).to_i
+    retryable do
+      (RemoteGem.where(Sequel.like(:name, "#{letter}%")).count / PER_PAGE).to_i
+    end
   end
 
   def each_of_letter(letter, page, &block)
     return enum_for(:each_of_letter, letter, page) unless block_given?
 
-    RemoteGem.where(Sequel.like(:name, "#{letter}%")).
-        limit(PER_PAGE, (page - 1) * PER_PAGE).each do |row|
-      yield row.name, to_versions(row)
+    retryable do
+      RemoteGem.where(Sequel.like(:name, "#{letter}%")).
+          limit(PER_PAGE, (page - 1) * PER_PAGE).each do |row|
+        yield row.name, to_versions(row)
+      end
     end
   end
 
   def pages_of_find_by(search)
-    (RemoteGem.where(Sequel.like(:name, "%#{search}%")).count / PER_PAGE).to_i
+    retryable do
+      (RemoteGem.where(Sequel.like(:name, "%#{search}%")).count / PER_PAGE).to_i
+    end
   end
 
   def find_by(search, page)
     return enum_for(:find_by, search, page) unless block_given?
 
-    RemoteGem.where(Sequel.like(:name, "%#{search}%")).
-        order { length(:name).asc }.
-        limit(PER_PAGE, (page - 1) * PER_PAGE).each do |row|
-      yield row.name, to_versions(row)
+    retryable do
+      RemoteGem.where(Sequel.like(:name, "%#{search}%")).
+          order { length(:name).asc }.
+          limit(PER_PAGE, (page - 1) * PER_PAGE).each do |row|
+        yield row.name, to_versions(row)
+      end
     end
   end
 
   private
+
+  def retryable(max_attempts = 15, &block)
+    attempts = 0
+    yield
+  rescue Sequel::DatabaseError => e
+    puts "Database error when writing versions (locked?): #{e.message}"
+
+    if attempts < max_attempts
+      attempts += 1
+      sleep(0.1 * attempts)
+      retry
+    end
+  end
 
   def name_eq(name)
     Sequel.lit("CAST(name as text) = CAST(? as text)", name)
