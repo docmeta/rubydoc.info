3873a3c3de18d43b1a516d0f339b3ee76c6f025e
Loren Segal <lsegal@soen.ca>
2023-04-12 11:25:24 -0700

    Generous timeout if database is not up yet

--- lib/db.rb
+++ lib/db.rb
@@ -1,1 +1,1 @@
-DB = Sequel.connect(ENV['DOCKERIZED'] ? 'postgres://db:db@db/db' : 'sqlite://data/remote_gems.sqlite')
+DB = Sequel.connect(ENV['DOCKERIZED'] ? 'postgres://db:db@db/db' : 'sqlite://data/remote_gems.sqlite', pool_timeout: 60)
