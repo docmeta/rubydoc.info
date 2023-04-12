daebb1b091ba16db775568223361e1d87d518c85
Loren Segal <lsegal@soen.ca>
2023-04-12 14:44:57 -0700

    Remove unused setup.rb

--- templates/default/fulldoc/html/setup.rb
+++
@@ -1,7 +0,0 @@
-# @fixme should be able to use Object.extend(T('layout')).menu_lists but for
-#        some reason it causes a crash.
-def menu_lists
-  [{:type => 'class', :title => 'Classes', :search_title => 'Class List'},
-    {:type => 'method', :title => 'Methods', :search_title => 'Method List'},
-    {:type => 'file', :title => 'Files', :search_title => 'File List'}]
-end
