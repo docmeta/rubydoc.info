c74dc947b7d367a303541a66da05cddf8e97ef7a
Loren Segal <lsegal@soen.ca>
2025-05-04 13:52:06 -0700

    Re-introduce update gem queue

--- config/puma/plugin/queue_jobs.rb
+++ config/puma/plugin/queue_jobs.rb
@@ -2,8 +2,11 @@ Puma::Plugin.create do
   def start(launcher)
     launcher.events.on_booted do
       if ENV["WITHOUT_JOBS"].blank?
-        # UpdateRemoteGemsListJob.perform_later
-        RegisterLibrariesJob.perform_later
+        [UpdateRemoteGemsListJob, RegisterLibrariesJob].each do |job_class|
+          if SolidQueue::Job.where(class_name: job_class.name).none?
+            job_class.perform_later
+          end
+        end
       end
     end
   end
