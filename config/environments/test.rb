503bfe5d030b2489815b60502548aac786b33d82
Loren Segal <lsegal@soen.ca>
2025-05-07 12:43:59 -0700

    Fix test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -31,10 +31,10 @@ Rails.application.configure do
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
-  config.action_mailer.delivery_method = :test
+  # config.action_mailer.delivery_method = :test
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "example.com" }
+  # config.action_mailer.default_url_options = { host: "example.com" }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
