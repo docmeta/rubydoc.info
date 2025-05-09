776c80d52c24f560960389e24721562902e757fd
Loren Segal <lsegal@soen.ca>
2025-05-08 17:49:52 -0700

    Fix focus and add esc on modal

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,7 +1,19 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  close() {
-    this.element.remove();
+  static targets = ["focus"];
+
+  connect() {
+    const el = this.hasFocusTarget ? this.focusTarget : this.element;
+    el.focus();
+  }
+
+  close(event) {
+    if (
+      (event.key === "Escape" && !event.target.value) ||
+      event.target === this.element
+    ) {
+      this.element.remove();
+    }
   }
 }
