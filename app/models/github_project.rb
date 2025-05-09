da1781a80526a8487a2dc150a4f8d1898dde82d7
Loren Segal <lsegal@soen.ca>
2025-05-08 17:50:52 -0700

    Fix URL format validation for projects with periods

--- app/models/github_project.rb
+++ app/models/github_project.rb
@@ -2,7 +2,7 @@ class GithubProject
   include ActiveModel::API
 
   attr_accessor :url, :commit
-  validates_format_of :url, with: %r{\Ahttps://github.com/[a-z0-9\-_]+/[a-z0-9\-_]+\z}i
+  validates_format_of :url, with: %r{\Ahttps://github.com/[a-z0-9\-_\.]+/[a-z0-9\-_\.]+\z}i
   validates_format_of :commit, with: /\A[0-9a-z_\.-]{1,40}\z/i, allow_blank: true
 
   def owner
